f9b074ad18e46f00f3ebc2de547c16a9dad39f88
taewan0530 <taewan0530@daum.net>
2021-10-16 16:12:25 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["IceCream"]),
     ],
     dependencies: [
-        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa", .upToNextMajor(from: "10.8.0"))
+        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa", .upToNextMajor(from: "10.7.0"))
     ],
     targets: [
         .target(
