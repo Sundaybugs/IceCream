5bdeb8e914895446fbe4397d93fe3de7e7a0a500
taewan0530 <taewan0530@daum.net>
2024-10-23 14:20:13 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/realm/realm-swift", 
+            url: "https://github.com/realm/realm-swift.git", 
             "4.1.1"..<"11.0.0"
         )
     ],
